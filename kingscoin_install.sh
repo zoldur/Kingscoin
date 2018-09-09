08dac19da85aee33da23c213be1c9acbb011edb1
Zoldur <zoldur99@gmail.com>
2018-09-09 14:34:05 +0100

    update

--- kingscoin_install.sh
+++ kingscoin_install.sh
@@ -21,7 +21,7 @@ NC='\033[0m'
 
 
 function download_node() {
-  echo -e "Prepare to download ${GREEN}$COIN_NAME${NC}."
+  echo -e "Downloading ${GREEN}$COIN_NAME${NC}."
   cd $TMP_FOLDER >/dev/null 2>&1
   wget -q $COIN_TGZ
   compile_error
